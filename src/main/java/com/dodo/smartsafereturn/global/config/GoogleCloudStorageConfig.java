3b32ed5393c81508afc02dfb792e4bb29c65ec6e
gyuchanlee <hoo788@gmail.com>
2025-04-01 18:56:39 +0900

    2025-04-01 gcp 배포 세팅 4

--- src/main/java/com/dodo/smartsafereturn/global/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/dodo/smartsafereturn/global/config/GoogleCloudStorageConfig.java
@@ -13,21 +13,29 @@ import java.io.IOException;
 
 @Configuration
 public class GoogleCloudStorageConfig {
-
-    @Value("${spring.cloud.gcp.storage.credentials.location}")
-    private String credentialsLocation;
+//
+//    @Value("${spring.cloud.gcp.storage.credentials.location}")
+//    private String credentialsLocation;
 
     @Value("${spring.cloud.gcp.storage.project-id}")
     private String projectId;
 
     @Bean
     public Storage storage() throws IOException {
-        String credentialsPath = ResourceUtils.getFile(credentialsLocation).getAbsolutePath();
+//        String credentialsPath = ResourceUtils.getFile(credentialsLocation).getAbsolutePath();
+//
+//        return StorageOptions.newBuilder()
+//                .setProjectId(projectId)
+//                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(credentialsPath)))
+//                .build()
+//                .getService();
 
-        return StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(credentialsPath)))
-                .build()
-                .getService();
+        // 배포환경을 위한 세팅
+        StorageOptions.Builder builder = StorageOptions.newBuilder();
+        if (projectId != null && !projectId.isEmpty()) {
+            builder.setProjectId(projectId);
+        }
+        // 기본 인증 매커니즘 활용
+        return builder.build().getService();
     }
 }
