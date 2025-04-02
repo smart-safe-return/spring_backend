01738e771dbda6bf48adcb84d471a40fbecb18a3
gyuchanlee <hoo788@gmail.com>
2025-04-02 15:45:27 +0900

    2025-04-02 클라우드 스토리지 - 등록 인증 오류 해결

--- src/main/java/com/dodo/smartsafereturn/global/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/dodo/smartsafereturn/global/config/GoogleCloudStorageConfig.java
@@ -6,6 +6,7 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.util.ResourceUtils;
 
 import java.io.FileInputStream;
@@ -20,21 +21,34 @@ public class GoogleCloudStorageConfig {
     @Value("${spring.cloud.gcp.storage.project-id}")
     private String projectId;
 
+    @Value("${spring.cloud.gcp.storage.credentials.location:}") // prod 환경일때는 빈 문자열
+    private String credentialsLocation;
+
     @Bean
+    @Profile("dev")
+    public Storage storageLocal() throws IOException {
+
+        String credentialsPath = ResourceUtils.getFile(credentialsLocation).getAbsolutePath();
+
+        return StorageOptions.newBuilder()
+                .setProjectId(projectId)
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(credentialsPath)))
+                .build()
+                .getService();
+
+    }
+
+    @Bean
+    @Profile("prod")
     public Storage storage() throws IOException {
-//        String credentialsPath = ResourceUtils.getFile(credentialsLocation).getAbsolutePath();
-//
-//        return StorageOptions.newBuilder()
-//                .setProjectId(projectId)
-//                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(credentialsPath)))
-//                .build()
-//                .getService();
 
         // 배포환경을 위한 세팅
         StorageOptions.Builder builder = StorageOptions.newBuilder();
+
         if (projectId != null && !projectId.isEmpty()) {
             builder.setProjectId(projectId);
         }
+
         // 기본 인증 매커니즘 활용
         return builder.build().getService();
     }
